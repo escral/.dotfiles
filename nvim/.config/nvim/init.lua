36a43060f269fc5fdf6984987170c725850dded3
Alexander Topalo <topaloalexander@gmail.com>
2026-02-28 01:01:17 +0200

    fix nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -35,6 +35,8 @@ I hope you enjoy your Neovim journey,
 
 P.S. You can delete this when you're done too. It's your config now :)
 --]]
+vim.islist = vim.islist or vim.tbl_islist
+
 local setKey = function(key, value, opts, modes)
   modes = modes or { 'n', 'i', 'v', 't' }
 
@@ -183,18 +185,11 @@ require('lazy').setup({
   },
 
   {
-    -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
+    main = 'ibl',
     opts = {
-      -- lua array of chars, which can be used as a fallback indent marker
-      indent_blankline_char = '|',
-      indent_blankline_char_blankline = '┆',
-
-      show_trailing_blankline_indent = false,
-      show_first_indent_level = true,
-      -- show_current_context = true,
+      indent = { char = '│' },
+      scope = { enabled = false },
     },
   },
 
@@ -205,11 +200,13 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   {
     'numToStr/Comment.nvim',
-
+    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
     config = function()
+      require('ts_context_commentstring').setup { enable_autocmd = false }
       require('Comment').setup({
         sticky = true,
         padding = true,
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
       })
       setKey('<C-/>', '<Plug>(comment_toggle_linewise_current)j', { noremap = false }, { 'n' })
       setKey('<C-/>', '<C-o><Plug>(comment_toggle_linewise_current)<C-o>j', { noremap = false }, { 'i' })
@@ -238,13 +235,56 @@ require('lazy').setup({
   },
 
   {
-    -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master', -- pin to old API; main branch requires Neovim 0.11+
+    build = ':TSUpdate',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
-      'JoosepAlviste/nvim-ts-context-commentstring',
-      build = ":TSUpdate",
     },
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        ensure_installed = { 'lua', 'tsx', 'typescript', 'javascript', 'vimdoc', 'vim', 'php', 'vue', 'scss', 'html', 'twig' },
+        auto_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<M-space>',
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = { [']m'] = '@function.outer', [']]'] = '@class.outer' },
+            goto_next_end = { [']M'] = '@function.outer', [']['] = '@class.outer' },
+            goto_previous_start = { ['[m'] = '@function.outer', ['[['] = '@class.outer' },
+            goto_previous_end = { ['[M'] = '@function.outer', ['[]'] = '@class.outer' },
+          },
+          swap = {
+            enable = true,
+            swap_next = { ['<leader>a'] = '@parameter.inner' },
+            swap_previous = { ['<leader>A'] = '@parameter.inner' },
+          },
+        },
+      }
+    end,
   },
 
   {
@@ -553,81 +593,6 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc 
 vim.keymap.set('n', '<C-r>', require('telescope.builtin').lsp_document_symbols, { desc = '[S]earch [S]ymbols' })
 vim.keymap.set('n', '<leader>sb', require('telescope.builtin').git_branches, { desc = '[S]earch [B]ranches' })
 
--- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
-require('nvim-treesitter.configs').setup {
-  -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'lua', 'tsx', 'typescript', 'javascript', 'vimdoc', 'vim', 'php', 'vue', 'scss', 'html', 'tsx',
-    'twig' },
-
-  context_commentstring = {
-    enable = true,
-  },
-
-  -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-  auto_install = false,
-
-  highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
-      scope_incremental = '<c-s>',
-      node_decremental = '<M-space>',
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
-    },
-  },
-}
-
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
@@ -717,7 +682,7 @@ local servers = {
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
-  tsserver = {},
+  ts_ls = {},
   html = {},
   cssls = {
     css = {
@@ -757,16 +722,15 @@ local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = servers[server_name],
+      }
+    end,
+  },
 }
 
 -- nvim-cmp setup
